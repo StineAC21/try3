dd6e477ed8870d0e6188a00b1c10a0cc5a878bc3
WEHRMANHM21 <wehrmanhm21@gcc.edu>
2022-02-23 09:51:57 -0500

    Changed to take name from user

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Enter your name");
+        Scanner scn = new Scanner(System.in);
+        String name = scn.next();
+        System.out.println("Hello " + name);
     }
 }
