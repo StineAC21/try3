45c68f3d4f2c776671b3ebd604e976d4ed1d6ef1
WEHRMANHM21 <wehrmanhm21@gcc.edu>
2022-02-25 09:25:16 -0500

    minor edit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("Enter your name");
         Scanner scn = new Scanner(System.in);
         String name = scn.next();
-        System.out.println("Hello " + name);
+        System.out.println("Hello, " + name);
     }
 }
